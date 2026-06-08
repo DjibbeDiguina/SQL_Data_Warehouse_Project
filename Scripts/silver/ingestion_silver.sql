fd82a20f52357f3d6f892c021e6812089e9a402c
Djibbe Christian Diguina <126919967+DjibbeDiguina@users.noreply.github.com>
2026-06-08 12:29:02 +0300

    Update ingestion script for CRM sales details

--- Scripts/silver/ingestion_silver.sql
+++ Scripts/silver/ingestion_silver.sql
@@ -70,3 +70,51 @@ FROM
 	(select *, ROW_NUMBER() OVER(PARTITION BY prd_id order by prd_start_dt) as flag_last
 	from bronze.crm_prd_info where prd_id is not null) t
 WHERE flag_last = 1;
+
+select count(*) from silver.crm_prd_info;
+
+/***********************************************SILVER CRM SALES DETAILS *************************************************************************/
+TRUNCATE TABLE silver.crm_sales_details;
+INSERT INTO silver.crm_sales_details
+(
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+)
+
+SELECT 
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+CASE
+	WHEN sls_order_dt <= 0 OR LEN(sls_order_dt) != 8 THEN NULL
+	ELSE CAST(CAST(sls_order_dt AS NVARCHAR(10)) AS DATE)
+END AS sls_order_dt,
+CASE
+	WHEN sls_ship_dt <= 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+	ELSE CAST(CAST(sls_ship_dt AS NVARCHAR(10)) AS DATE)
+END AS sls_ship_dt,
+CASE
+	WHEN sls_due_dt <= 0 OR LEN(sls_due_dt) != 8 THEN NULL
+	ELSE CAST(CAST(sls_due_dt AS NVARCHAR(10))AS DATE)
+END AS sls_due_dt,
+CASE
+	WHEN sls_sales <= 0 OR sls_sales IS NULL THEN ABS(sls_quantity * sls_price)
+	WHEN sls_sales != sls_quantity * sls_price THEN ABS(sls_quantity * sls_price)
+	ELSE sls_sales
+END as sls_sales,
+sls_quantity,
+CASE
+	WHEN sls_price < 0 THEN ABS(sls_sales)
+	WHEN sls_price IS NULL OR sls_price = 0 THEN ABS(sls_sales/NULLIF(sls_quantity, 0))
+	ELSE sls_price
+END AS sls_price
+FROM bronze.crm_sales_details;
+
+select count(*) from silver.crm_sales_details;
